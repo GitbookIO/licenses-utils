12f18568ec1d7bb68da72c9bcb3d188df5a0f4e3
Samy Pessé <samypesse@gmail.com>
2014-08-20 12:16:30 -0700

    Improve detection of license
    Add test for apache

--- test/detect.js
+++ test/detect.js
@@ -3,20 +3,29 @@ var fs = require('fs');
 var path = require('path');
 var licenses = require('../lib');
 
+
+var testFolder = function(folder, result, done) {
+    licenses.detectFolder(folder, function(err, license) {
+        if (err) throw err;
+
+        if (!license || license.id != result) {
+            throw "Invalid license";
+        }
+
+        done();
+    });
+}
+
 describe('Detect', function () {
     it('should detect from string', function() {
         assert((licenses.detect(fs.readFileSync(path.join(__dirname, "./fixtures/mit/LICENSE")).toString()).id == "mit"));
     });
 
-    it('should detect from folder', function(done) {
-        licenses.detectFolder(path.join(__dirname, "./fixtures/mit"), function(err, license) {
-            if (err) throw err;
+    it('should detect MIT', function(done) {
+        testFolder(path.join(__dirname, "./fixtures/mit"), "mit", done);
+    });
 
-            if (license.id != "mit") {
-                throw "Invalid license";
-            }
-
-            done();
-        });
+    it('should detect Apache', function(done) {
+        testFolder(path.join(__dirname, "./fixtures/apache"), "apache-2", done);
     });
 });
