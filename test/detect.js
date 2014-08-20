feaa2a68b935476d4034e91ef00b3e03d8656e05
Samy Pessé <samypesse@gmail.com>
2014-08-20 11:21:13 -0700

    Add detection from project

--- test/detect.js
+++ test/detect.js
@@ -7,4 +7,16 @@ describe('Detect', function () {
     it('should detect from string', function() {
         assert((licenses.detect(fs.readFileSync(path.join(__dirname, "./fixtures/mit/LICENSE")).toString()).id == "mit"));
     });
+
+    it('should detect from folder', function(done) {
+        licenses.detectFolder(path.join(__dirname, "./fixtures/mit"), function(err, license) {
+            if (err) throw err;
+
+            if (license.id != "mit") {
+                throw "Invalid license";
+            }
+
+            done();
+        });
+    });
 });
